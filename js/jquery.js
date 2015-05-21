3934b6dd7e3c273030565f1241d4d86ff815595e
karthika <karthikam@qburst.com>
2015-05-21 15:34:13 +0530

    Modified and jquery added

---
+++ js/jquery.js
@@ -0,0 +1,288 @@
+$(document).ready(function(){
+	$photoessay = $('.slide-img');  
+	var $x = [];
+
+	n = $('.slide-img>ul').length+1;
+	for(var i=1; i<n;i++){ 
+		$x[i] = $('.slide-img ul.multi-links:nth-child('+i+')');
+		console.log($x[i]);
+	}
+
+	$phfirstchild = $photoessay.children().first().index();
+	$phlastchild = $photoessay.children().last().index();
+	$multil1 = $('.slide-img ul.multi-links:nth-child(1)');
+
+	if($photoessay.children().length > 1){
+
+		$('.next').hover(function(){
+			$(this).css({'background-color':'#e7d4cb'});
+		},function(){
+			$(this).css({'background-color':'#f7f1ee'});
+		});
+
+		$('.prev').hover(function(){
+			$(this).css({'background-color':'#f7f1ee'});
+		});
+
+		$multil2 = $('.slide-img ul.multi-links:nth-child(2)');
+		$multil3 = $('.slide-img ul.multi-links:nth-child(3)');
+
+		$multil1.addClass('enter active');
+		$multil2.addClass('wait disabled');
+		$multil3.addClass('leave disabled');
+
+		// $('.photoessay-links>ul.multi-links:nth-child(0)').hide();
+
+		$('.next').click(function(){
+			if($multil1.hasClass('active') || $multil1.hasClass('enter')){
+
+				$selectnext = $multil1.index();
+
+				$multil1.removeClass();
+				$multil2.removeClass();
+				$multil3.removeClass();
+
+				$('.prev').hover(function(){
+					$(this).css({'background-color':'#e7d4cb'});
+				},function(){
+					$(this).css({'background-color':'#f7f1ee'});
+				});
+
+				// $multil1.toggleClass('enter active leave disabled');
+				// $multil2.toggleClass('wait disabled enter active');
+				// $multil3.toggleClass('leave wait');
+
+				$multil1.addClass('multi-links leave disabled');
+				$multil2.addClass('multi-links enter active');
+				$multil3.addClass('multi-links wait disabled');
+			}
+			else if($multil2.hasClass('active') || $multil2.hasClass('enter')){
+
+				$selectnext = $multil2.index()+1;
+
+				if($phlastchild == $selectnext){
+
+					$(".next").prop('disabled',true);
+					$('.next').hover(function(){
+						$(this).css({'background-color':'#f7f1ee'});
+					}); 
+
+					// $multil1.toggleClass('leave wait');
+					// $multil1.toggleClass('wait disabled enter active');
+					// $multil2.toggleClass('enter active wait disabled');
+					// $multil3.toggleClass('leave disabled enter active');
+
+					// $setTimeout(function() {$multil1.toggleClass('multi-links leave wait disabled'); }, 0.2);
+					
+					// $multil1.addClass('multi-links enter active');
+					// $multil2.addClass('multi-links wait disabled');
+					// $multil3.addClass('multi-links wait disabled');
+				}
+
+				$multil1.removeClass();
+				$multil2.removeClass();
+				$multil3.removeClass();	
+
+				$multil1.addClass('multi-links wait disabled');
+				$multil2.addClass('multi-links leave disabled');
+				$multil3.addClass('multi-links enter active');
+				// $multil1.addClass('leave wait');
+				// $multil2.addClass('enter active leave disabled');
+				// $multil3.addClass('wait disabled enter active');
+			}
+
+			// else if($multil3.hasClass('active') || $multil3.hasClass('enter')){
+
+			// 	$selectnext = $multil3.index(); 
+
+			// 	if($phlastchild == $selectnext){
+			// 		// $multil1.toggleClass('wait disabled enter active');
+			// 		// $multil2.toggleClass('leave wait');
+			// 		// $multil3.toggleClass('leave disabled enter active');
+
+			// 		$(".next").prop('disabled',true);
+			// 		$('.next').hover(function(){
+			// 			$(this).css({'background-color':'#f7f1ee'});
+			// 		});
+			// 	}
+			// 	else{
+					
+			// 		$multil1.removeClass();
+			// 		$multil2.removeClass();
+			// 		$multil3.removeClass();	
+
+			// 		$multil1.addClass('multi-links wait disabled');
+			// 		$multil2.addClass('multi-links leave disabled');
+			// 		$multil3.addClass('multi-links enter active');
+
+			// 		// $multil1.toggleClass('wait disabled enter active');
+			// 		// $multil2.toggleClass('leave wait');
+			// 		// $multil3.toggleClass('leave disabled enter active');
+			// 	}
+				
+			// }
+		});	
+
+		$('.prev').click(function(){
+			if($multil3.hasClass('active') || $multil3.hasClass('enter')){
+
+				$multil1.removeClass();
+				$multil2.removeClass();
+				$multil3.removeClass();	
+
+				$multil1.addClass('multi-links leave disabled');
+				$multil2.addClass('multi-links enter active');
+				$multil3.addClass('multi-links wait disabled');
+			}
+			else if($multil2.hasClass('active') || $multil2.hasClass('enter')){
+
+				$selectprev = $multil2.index()-1;
+
+				if($selectprev == $phfirstchild){  
+					$('.prev').hover(function(){
+						$(this).css({'background-color':'#f7f1ee'});
+					});
+					$('.next').hover(function(){
+						$(this).css({'background-color':'#e7d4cb'});
+					},function(){
+						$(this).css({'background-color':'#f7f1ee'});
+					});
+				}
+
+				$multil1.removeClass();
+				$multil2.removeClass();
+				$multil3.removeClass();	
+
+				$multil1.addClass('multi-links enter active');
+				$multil2.addClass('multi-links wait active');
+				$multil3.addClass('multi-links wait disabled');
+			}
+			// else if($multil1.hasClass('active') || $multil1.hasClass('enter')){
+
+			// 	$selectprev = $multil1.index()-1;
+
+			// 	console.log('$selectprev addasdasdasdas'+$selectprev);
+			// 	console.log('$phfirstchild'+$phfirstchild);
+
+			// 	if($selectprev == $phfirstchild){ 
+			// 		$('.prev').hover(function(){
+			// 			$('.prev').css({'background-color':'#f7f1ee'});
+			// 		});
+			// 	}
+			// }
+		});	
+	}
+
+	else{
+		$('.next').hover(function(){
+			$(this).css({'background-color':'#f7f1ee'});
+		});
+		$('.prev').hover(function(){
+			$(this).css({'background-color':'#f7f1ee'});
+		});
+	}
+});
+
+
+// $.fn.removeClasses = function()
+// {
+// 	$multil1.removeClass();
+// 	$multil2.removeClass();
+// 	$multil3.removeClass();	
+// }
+// $photoessay.removeClasses();
+
+
+// $(document).ready(function(){
+// 	$photoessay = $('.multi-links');
+// 	$phlastchild = $('.multi-links').children().last().attr('id');
+// 	$multil1 = $('.multi-links:nth-child(1)');
+// 	$multil2 = $('.multi-links:nth-child(2)');
+// 	$multil3 = $('.multi-links:nth-child(3)');
+
+// 	console.log($multil1);
+// 	console.log($multil2);
+// 	console.log($multil3);
+
+// 	$multil1.addClass('enter active');
+// 	$multil2.addClass('wait disabled');
+// 	$multil3.addClass('wait disabled');
+
+// 	$('.right-scroll-move11').click(function(){
+// 		if($multil1.hasClass('active') || $multil1.hasClass('enter')){
+			
+// 			$multil1.removeClass();
+// 			$multil2.removeClass();
+// 			$multil3.removeClass();
+
+// 			$multil1.addClass('leave disabled');
+// 			$multil2.addClass('enter active');
+// 			$multil3.addClass('wait disabled');
+
+// 			$select = $multil1.attr('id');
+// 		}
+// 		else if($multil2.hasClass('active') || $multil2.hasClass('enter')){
+
+// 			$multil1.removeClass();
+// 			$multil2.removeClass();
+// 			$multil3.removeClass();
+
+
+// 			$multil1.addClass('wait disabled');
+// 			$multil2.addClass('leave disabled');
+// 			$multil3.addClass('enter active');
+
+// 			$select = $multil2.attr('id');
+// 		}
+// 		else if($multil3.hasClass('active') || $multil3.hasClass('enter')){
+// 			// $multil1.removeClass();
+// 			// $multil2.removeClass();
+// 			// $multil3.removeClass();
+
+// 			// $multil1.addClass('enter active');
+// 			// $multil2.addClass('wait disabled');
+// 			// $multil3.addClass('leave disabled');
+
+// 			// $select = $multil2.attr('id');
+
+// 			if($phlastchild == $select){
+
+// 			$multil1.removeClass();
+// 			$multil2.removeClass();
+// 			$multil3.removeClass();
+
+// 			$multil1.addClass('enter active');
+// 			$multil2.addClass('wait disabled');
+// 			$multil3.addClass('wait disabled');
+
+// 			console.log('sdad');
+// 		}
+
+// 		if($phlastchild == $select){
+
+// 			$multil1.removeClass();
+// 			$multil2.removeClass();
+// 			$multil3.removeClass();
+
+// 			$multil1.addClass('enter active');
+// 			$multil2.addClass('wait disabled');
+// 			$multil3.addClass('wait disabled');
+
+// 			console.log('sdad');
+// 		}
+
+// 		// console.log($phlastchild);
+// 		// console.log($multil1.attr('id'));
+// 		// console.log($('#photoessay-links').children());
+// 	});
+// });
+
+
+// $.fn.addremovefns = function(myid)
+// {
+// 	$myid.addClass('enter active');
+// }
+// $('#myId').addremovefns($multil1);
+
+
+
---
+++ js/verge-js.js
@@ -0,0 +1,143 @@
+search = document.getElementById("search");
+drop = document.getElementById("header-dropdown");
+
+window.onscroll = function () {
+            	scroll = document.getElementById("header-links");
+            	triangle=document.getElementById("triangle-icon");
+                if(window.pageYOffset>100 && window.pageYOffset< 1500){               	
+                	scroll.className= "lowerheader-static";
+                	triangle.style.display = "initial";
+                }
+                else{
+                	scroll.className= "lowerheader-normal";
+                	triangle.style.display = "none";
+                }	
+            }
+
+function rightscrollnav(){
+	// photoessay = document.getElementById('photoessay-links');
+	// multil1 = document.getElementById('multi-link1');
+	// multil2 = document.getElementById('multi-link2');
+	// multil3 = document.getElementById('multi-link3');
+
+	// if(multil1.className == "enter active"){
+	// 	multil1.className = "leave disabled";
+	// 	multil2.className = "enter active";
+	// 	multil3.className = "wait disabled";
+	// }
+	// else if (multil2.className == "enter active"){		
+	// 	multil2.className = "leave disabled";
+	// 	multil3.className = "enter active";
+	// 	multil1.className = "wait disabled";
+	// }
+	// else if (multil3.className == "enter active"){		
+	// 	multil3.className = "leave disabled";
+	// 	multil1.className = "enter active";
+	// 	multil2.className = "wait disabled";
+	// }
+
+	
+	// else if (photoessay.lastChild){
+	// 	multil1.className = "enter active";
+	// 	multil2.className = "wait disabled";
+	// 	multil3.className = "wait disabled";
+	// }
+	// console.log(photoessay.children.lastElementChild);
+
+	// while (lastChildElement && lastChildElement.nodeType !== 1) {
+	//    lastChildElement = lastChildElement.previousSibling;
+	// }
+
+	// if (lastChildElement) {
+	//     // Do something
+	// }
+}
+
+function searchonclick(){	
+	 if(search.style.display=="none" || search.style.display==""){
+	 	search.style.display="initial";
+	 	search.value="";
+	 }
+	 else{
+	 	search.style.display="none";
+	 	// drop.style.display="none";
+	 	search.value="";
+	 }
+
+	 if(drop.style.display!="none"){
+	 	drop.style.display = "none";
+	 }
+
+}
+
+function droponclick(){
+	// drop = document.getElementById("header-dropdown");
+	 if(drop.style.display=="none" || drop.style.display==""){
+	 	drop.style.display="initial";
+	 	drop.value="";
+	 }
+	 else{
+	 	drop.style.display="none";
+	 	// search.style.display="none";
+	 	drop.value="";
+	 }
+	 if(search.style.display!="none"){
+	 	search.style.display = "none";
+	 }
+}
+
+function rightscroll(){
+	scroll2 = document.getElementById("multi-photoessay-content2");
+	scroll1 = document.getElementById("multi-photoessay-content1");
+
+	// if(scroll2.className == "hide" || scroll2.className == ""){
+	// 	scroll2.className = "show";
+	// 	scroll2.style.display = "initial";
+	// 	scroll1.className = "hide";
+	// }
+	// else if(scroll2.className == "show"){
+	// 	scroll1.className = "show";
+	// 	scroll1.style.display = "initial";
+	// 	scroll2.className = "hide";
+	// 	scroll2.style.display = "none";
+	// }
+	// else{
+	// 	scroll1.className = "show";
+	// 	scroll2.className = "hide";
+	// }
+
+
+}
+
+function storyrightscroll1(){
+	scrollsec1 = document.getElementById('multi-content-sec4');
+	if(scrollsec1.style.display == "none" || scrollsec1.style.display == ""){
+		scrollsec1.style.display = "initial";
+	}	
+	else{
+		scrollsec1.style.display = "none";
+	}
+
+	// scrollsec1 = document.getElementById('multi-content-sec1');//.getElementsByClassName('multi-content1');
+	// console[0].log(scrollsec1);
+}
+
+function storyrightscroll2(){
+	scrollsec2 = document.getElementById('multi-content-sec5');
+	if(scrollsec2.style.display == "none" || scrollsec2.style.display == ""){
+		scrollsec2.style.display = "initial";
+	}	
+	else{
+		scrollsec2.style.display = "none";
+	}
+}
+
+function storyrightscroll3(){
+	scrollsec3 = document.getElementById('multi-content-sec6');
+	if(scrollsec3.style.display == "none" || scrollsec3.style.display == ""){
+		scrollsec3.style.display = "initial";
+	}	
+	else{
+		scrollsec3.style.display = "none";
+	}
+}
